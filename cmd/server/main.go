d8718ac6d013602b02fdc8f632131fb2e914fba0
anastasiaboshnyakovich <anastasiaboshnyakovich@xpate.com>
2022-05-06 02:43:07 +0300

    Fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,80 @@
 package main
 
 import (
-	"github.com/DmitryKhitrin/alerting-service/internal/server"
+	"context"
+	"fmt"
+	"github.com/DmitryKhitrin/alerting-service/internal/server/config"
+	"github.com/DmitryKhitrin/alerting-service/internal/server/metrics"
+	metricsHandler "github.com/DmitryKhitrin/alerting-service/internal/server/metrics/handler"
+	metricsService "github.com/DmitryKhitrin/alerting-service/internal/server/metrics/service"
+	"github.com/DmitryKhitrin/alerting-service/internal/server/repositories"
+	"github.com/caarlos0/env/v6"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
-func main() {
-	if err := server.LaunchServer(); err != nil {
-		log.Fatalf("%s", err.Error())
+type App struct {
+	metricsService metrics.Service
+}
+
+func NewApp(cfg *config.Config) *App {
+	repository := repositories.NewLocalStorageRepository(cfg)
+
+	return &App{
+		metricsService: metricsService.NewMetricsService(repository),
 	}
+}
+
+func getRouter(a *App) *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	metricsHandler.RegisterHTTPEndpoints(router, a.metricsService)
+	return router
+}
+
+func main() {
+
+	cfg := config.Config{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	app := NewApp(&cfg)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	srv := &http.Server{
+		Addr:    cfg.Address,
+		Handler: getRouter(app),
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := srv.ListenAndServe(); err != nil {
+			cancel()
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-ctx.Done():
+		log.Println("context was canceled")
+	case s := <-quit:
+		log.Println("signal was provided: ", s)
+		cancel()
+	}
+
 }
