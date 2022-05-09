b234e712ae54b336ee18d81346e8c653d06b4a9a
khitrind <khitrind@yandex-team.ru>
2022-05-09 17:38:12 +0300

    Increment-6: Self review

--- internal/server/server.go
+++ internal/server/server.go
@@ -58,11 +58,12 @@ func LaunchServer() error {
 			log.Fatalf("Failed to listen and serve: %+v", err)
 		}
 	}()
-	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 
+	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
 	defer shutdown()
 
 	return srv.Shutdown(ctx)
